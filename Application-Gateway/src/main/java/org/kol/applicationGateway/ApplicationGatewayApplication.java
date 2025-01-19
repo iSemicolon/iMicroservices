386812847cd8cbeff12494c1f4ee33c857fce5c2
iSemicolon <palash.samanta7497@gmail.com>
2025-01-19 22:44:42 +0530

    "Discovery Service and Application-Gateway Created"

---
+++ Application-Gateway/src/main/java/org/kol/applicationGateway/ApplicationGatewayApplication.java
@@ -0,0 +1,16 @@
+package org.kol.applicationGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApplicationGatewayApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ApplicationGatewayApplication.class, args);
+    }
+
+}
---
+++ Application-Gateway/src/test/java/org/kol/applicationGateway/ApplicationGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kol.applicationGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
